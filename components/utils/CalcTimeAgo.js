6fb6d0801c3d2bbf1bbc1bc39563a2b0b75cbd37
Levyn Schneider <lyschneider@icloud.com>
2024-08-07 23:16:20 +0200

    Add more step down versions

--- components/utils/CalcTimeAgo.js
+++ components/utils/CalcTimeAgo.js
@@ -6,6 +6,15 @@ export default function CalcTimeAgo(date) {
   const minutes = Math.floor(seconds / 60);
   const hours = Math.floor(minutes / 60);
   const days = Math.floor(hours / 24);
+  const months = Math.floor(days / 30);
+  const years = Math.floor(months / 12);
+
+  if (years > 0) {
+    return `vor ${years} Jahr${years > 1 ? "en" : ""}`;
+  }
+  if (months > 0) {
+    return `vor ${months} Monat${months > 1 ? "en" : ""}`;
+  }
   if (days > 0) {
     return `vor ${days} Tag${days > 1 ? "en" : ""}`;
   }
